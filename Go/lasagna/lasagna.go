bc722ca6eb3ea44f5f1a4ce0be196504937495fb
jbaker <jared.baker@secondfront.com>
2022-10-30 18:45:14 -0600

    fix: use return instead of print

--- Go/lasagna/lasagna.go
+++ Go/lasagna/lasagna.go
@@ -2,25 +2,23 @@ package lasagna
 
 // TODO: define the 'OvenTime' constant
 const OvenTime = 40
-var numberOfLayers = 0
-var actualMinutesInOven = 0
 
 // RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
 func RemainingOvenTime(actualMinutesInOven int) int {
-	print(OvenTime - actualMinutesInOven)
+	return OvenTime - actualMinutesInOven
 
 	panic("RemainingOvenTime not implemented")
 }
 
 // PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
 func PreparationTime(numberOfLayers int) int {
-	print(2 * numberOfLayers)
+	return 2 * numberOfLayers
 
 	panic("PreparationTime not implemented")
 }
 
 // ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
 func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
-	print((numberOfLayers * 2) + actualMinutesInOven)
+	return (numberOfLayers * 2) + actualMinutesInOven
 	panic("ElapsedTime not implemented")
 }
