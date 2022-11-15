865c6218dd244a08e86f95f3453a3a5e75773e76
Jared Baker <jared.baker@secondfront.com>
2022-11-14 21:02:15 -0700

    Create race-track.go
    WIP

---
+++ Go/race-track.go
@@ -0,0 +1,61 @@
+package speed
+
+// TODO: define the 'Car' type struct
+type Car struct {
+    speed			int
+    batteryDrain	int
+    battery			int
+    distance		int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+    	return Car{
+            speed,
+            batteryDrain,
+            100,
+            0,
+        }
+	panic("Please implement the NewCar function")
+}
+
+// TODO: define the 'Track' type struct
+type Track struct{
+    distance int
+}
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+    return Track{
+        distance: distance,
+    }
+	panic("Please implement the NewTrack function")
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+    car.battery = battery - batteryDrain
+    car.distance = distance + speed
+    car.speed = speed
+    car.battery = battery
+    
+    car := Car{
+        speed: car.speed,
+        batteryDrain: car.batteryDrain,
+        battery: car.battery,
+        distance: car.distance,        
+    }
+    return Car(car)
+
+    panic("Please implement the Drive function")
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+    if Car[battery] > 2 && (Track[distance] - Car[distance] != 0) {
+        return true
+    } else {
+    	return false
+    }
+	panic("Please implement the CanFinish function")
+}
