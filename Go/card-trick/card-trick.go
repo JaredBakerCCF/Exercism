a0cc1404cc5e9e086c3972064b43c657dd6ebce9
Jared Baker <jared.baker@secondfront.com>
2022-11-17 11:13:43 -0700

    Update card-trick.go

--- Go/card-trick/card-trick.go
+++ Go/card-trick/card-trick.go
@@ -4,28 +4,47 @@ package cards
 func FavoriteCards() []int {
 	t := []int{2, 6, 9}
     return t
-
+    
 	panic("Please implement the FavoriteCards function")
 }
 
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
+	if index < len(slice) && index >= 0 {
+        return slice[index]
+    } else {
+    	return -1
+    }
 	panic("Please implement the GetItem function")
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
+    if index < len(slice) && index <= 0 {
+    	slice[index] = value
+        return slice
+    } else {
+    	return append(slice, index, value)
+    }
 	panic("Please implement the SetItem function")
 }
 
 // PrependItems adds an arbitrary number of values at the front of a slice.
 func PrependItems(slice []int, values ...int) []int {
+	return append(slice, values ...)
 	panic("Please implement the PrependItems function")
 }
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	panic("Please implement the RemoveItem function")
+	if (isIndexOutOfBounds(slice, index)) {
+		return slice
+	}
+	return append(slice[:index], slice[index + 1:]...)
+}
+
+func isIndexOutOfBounds (slice []int, index int) bool {
+	return index < 0 || index >= len(slice)
 }
